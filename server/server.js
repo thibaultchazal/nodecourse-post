98c828fba0847fe93f58e46130328c3ba84942a8
thibaultchazal <chazalthibault@gmail.com>
2018-04-17 16:33:27 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -100,6 +100,20 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body= _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user); //we have to send the token back as an http response header, this is why we use header. X-... specify a custom header
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
